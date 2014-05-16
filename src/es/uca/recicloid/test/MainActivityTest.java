fff65d04a3bebd2c63316bed3bd627af333a4a29
Diego Rubio <dbiosag@gmail.com>
2014-05-16 03:33:15 +0200

    Creates two initial bateries of tests, and test in Samsung Galaxy S2

---
+++ src/es/uca/recicloid/test/MainActivityTest.java
@@ -0,0 +1,26 @@
+package es.uca.recicloid.test;
+
+import es.uca.recicloid.activities.MainActivity;
+import android.test.ActivityInstrumentationTestCase2;
+
+public class MainActivityTest 
+	extends ActivityInstrumentationTestCase2<MainActivity>{
+	private MainActivity mMainActivityTest;
+	
+	public MainActivityTest(){
+		super(MainActivity.class);
+	}
+	
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+		mMainActivityTest = this.getActivity();
+	}
+
+	protected void tearDown() throws Exception {
+	}
+	
+	public void testMainActivity() {
+	    assertTrue(true);
+	}
+}
